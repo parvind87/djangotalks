8698f4fd84d9f3da3c6c92f55c11bff51645b761
Parvind kumar <parvindkumar@Parvinds-MacBook-Pro.local>
2023-01-28 23:15:41 +0530

    login logout in nav

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Welcome {username}, your account is created')
-            return redirect('/')
+            return redirect('login')
     else:
         form = RegisterForm()
     return render(request,'users/register.html', {'form':form})
